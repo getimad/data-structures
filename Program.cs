547aec3b9b12b081efdc20dbea038daba2252953
Imad Ez-zahi <getimad.pro@outlook.com>
2023-09-22 21:03:01 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
