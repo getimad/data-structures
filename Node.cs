4500e53be1c2bc4b7b14778bd35af8165e1196c7
Imad Ez-zahi <getimad.pro@outlook.com>
2023-11-03 21:52:50 +0100

    Add Node class
    - Separate Node class into its own file.

---
+++ Node.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructures
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node? Next { get; set; }
+
+        public Node(int value = 0, Node? next = null)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}
