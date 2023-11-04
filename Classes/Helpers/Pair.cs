4e5c6bce635f02b28c3e1a618ce0f3e5ad0eaf82
Imad Ez-zahi <getimad.pro@outlook.com>
2023-11-04 16:49:29 +0100

    Add Pair helper to Helpers directory

---
+++ Classes/Helpers/Pair.cs
@@ -0,0 +1,14 @@
+﻿namespace DataStructures.Classes.Helpers
+{
+    public class Pair
+    {
+        public int Key { get; set; }
+        public int Value { get; set; }
+
+        public Pair(int key, int value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+}
