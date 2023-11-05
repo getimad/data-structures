423e3dfd6be7c6aeeaf8dc35be1167483e90d43f
Imad Ez-zahi <getimad.pro@outlook.com>
2023-11-05 02:13:18 +0100

    Add Tree helper to Helpers directory

---
+++ Classes/Helpers/Tree.cs
@@ -0,0 +1,16 @@
+﻿namespace DataStructures.Classes.Helpers
+{
+    public class Tree
+    {
+        public int Value { get; set; }
+        public Tree? Left { get; set; }
+        public Tree? Right { get; set; }
+
+        public Tree(int val = 0, Tree? left = null, Tree? right = null)
+        {
+            Value = val;
+            Left = left;
+            Right = right;
+        }
+    }
+}
